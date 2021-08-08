94f23e09f6aa4c59aa244801f75a1aba6002453f
linchao <linchao.superlin@bytedance.com>
2021-08-08 22:36:13 +0800

    实现客户端

--- server/cache/inmemory.go
+++ server/cache/inmemory.go
@@ -1,9 +1,6 @@
 package cache
 
-import (
-	"log"
-	"sync"
-)
+import "sync"
 
 type inMemoryCache struct {
 	c     map[string][]byte
@@ -14,35 +11,26 @@ type inMemoryCache struct {
 func (c *inMemoryCache) Set(k string, v []byte) error {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-
 	tmp, exist := c.c[k]
 	if exist {
 		c.del(k, tmp)
 	}
-
-	log.Println("set key:{}, value:{}", k, v)
 	c.c[k] = v
 	c.add(k, v)
 	return nil
 }
 
 func (c *inMemoryCache) Get(k string) ([]byte, error) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	log.Println("get key:{}", k)
-
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
 	return c.c[k], nil
 }
 
 func (c *inMemoryCache) Del(k string) error {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-
 	v, exist := c.c[k]
-
 	if exist {
-		log.Println("delete key:{}", k)
 		delete(c.c, k)
 		c.del(k, v)
 	}
@@ -54,9 +42,5 @@ func (c *inMemoryCache) GetStat() Stat {
 }
 
 func newInMemoryCache() *inMemoryCache {
-	return &inMemoryCache{
-		make(map[string][]byte),
-		sync.RWMutex{},
-		Stat{},
-	}
+	return &inMemoryCache{make(map[string][]byte), sync.RWMutex{}, Stat{}}
 }
